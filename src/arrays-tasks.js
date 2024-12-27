2cbeb362c70ff451ec99b564962dbfa764c257f6
ilonaGold <songa17@inbox.lv>
2024-12-27 19:11:01 +0200

    feat: implement getIdentityMatrix function

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -411,8 +411,12 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (__, outerIndex) =>
+    Array.from({ length: n }, (_, innerIndex) =>
+      outerIndex === innerIndex ? 1 : 0
+    )
+  );
 }
 
 /**
